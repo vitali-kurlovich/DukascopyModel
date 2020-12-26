43d74af8b1ecd39938a75c17c3d8a9fb7bbf68bb
Vitali Kurlovich <vitalikurlovich@gmail.com>
2020-12-26 20:08:13 +0300

    Update Finstruments.swift

--- Sources/DukascopyModel/Finstruments.swift
+++ Sources/DukascopyModel/Finstruments.swift
@@ -8,17 +8,24 @@
 import Foundation
 
 public
-struct Finstruments: Codable {
-    public
-    struct Group: Codable {
+struct Finstruments: Equatable {
+    public let instruments: [String: Instrument]
+    public let groups: [String: Group]
+}
+
+public
+extension Finstruments {
+    struct Group: Equatable {
         public let id: String
         public let title: String
         public let parents: String?
         public let instruments: [String]?
     }
+}
 
-    public
-    struct Instrument: Codable {
+public
+extension Finstruments {
+    struct Instrument: Equatable {
         public let title: String
         public let special: Bool
         public let name: String
@@ -37,27 +44,5 @@ struct Finstruments: Codable {
 
         public let commoditiesPerContract: Int?
         public let unit: String?
-
-        enum CodingKeys: String, CodingKey {
-            case title
-            case special
-            case name
-            case description
-            case filename = "historical_filename"
-            case pipValue
-            case baseCurrency = "base_currency"
-            case quoteCurrency = "quote_currency"
-            case tags = "tag_list"
-            case historyStartTick = "history_start_tick"
-            case historyStart10sec = "history_start_10sec"
-            case historyStartMinute = "history_start_60sec"
-            case historyStartHour = "history_start_60min"
-            case historyStartDay = "history_start_day"
-            case commoditiesPerContract = "commodities_per_contract"
-            case unit
-        }
     }
-
-    public let instruments: [String: Instrument]
-    public let groups: [String: Group]
 }
