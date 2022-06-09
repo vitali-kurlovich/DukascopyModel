f3fbe3a94455eaed6415d3b64ede195a37bae216
vitali-kurlovich <vitalikurlovich@gmail.com>
2022-06-10 01:20:32 +0300

    Update Group.swift

--- Sources/DukascopyModel/Group.swift
+++ Sources/DukascopyModel/Group.swift
@@ -24,7 +24,38 @@ public struct Group: Equatable {
     }
 }
 
-extension Group: Codable {}
+extension Group: Codable {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case instruments
+        case groups
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = try container.decode(String.self, forKey: .id)
+        title = try container.decode(String.self, forKey: .title)
+        instruments = try container.decode([Instrument]?.self, forKey: .instruments) ?? []
+        groups = try container.decode([Group]?.self, forKey: .groups) ?? []
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(id, forKey: .id)
+        try container.encode(title, forKey: .title)
+
+        if !instruments.isEmpty {
+            try container.encode(instruments, forKey: .instruments)
+        }
+
+        if !groups.isEmpty {
+            try container.encode(groups, forKey: .groups)
+        }
+    }
+}
 
 extension Group: Sequence {
     public typealias Element = Instrument
